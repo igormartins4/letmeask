487512da516a5cefb7cc8fad5f70327e43fbc448
Igor Martins <iguinhoigor4@gmail.com>
2021-06-22 21:15:16 -0300

    fix: Corrigir nome de variavéis

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,6 +1,5 @@
-import ilustrationImg from '../assets/images/illustration.svg';
+import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button';
 
@@ -10,7 +9,7 @@ export function NewRoom() {
     return (
         <div id="page-auth">
             <aside>
-                <img src={ilustrationImg} alt="Ilustração simnbolizando perguntas e respostas" />
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
                 <strong>Crie salas de Q&amp;A ao vivo</strong>
                 <p>Tire as dúvidas da sua audiência em tempo real</p>
             </aside>
