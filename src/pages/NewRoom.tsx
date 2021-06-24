b10b253540bbc360aaf70b21a995b391c31dbd71
Igor Martins <iguinhoigor4@gmail.com>
2021-06-23 22:32:20 -0300

    feat: Tirar evento padrão do formulário

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg';
@@ -10,6 +11,17 @@ import '../styles/auth.scss';
 
 export function NewRoom() {
     //const { user } = useAuth();
+
+    const [newRoom, setNewRoom] = useState('');
+
+    async function handleCreateRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if(newRoom.trim() === '') {
+            return;            
+        }
+    }
+
     return (
         <div id="page-auth">
             <aside>
@@ -21,10 +33,12 @@ export function NewRoom() {
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
                     <h2>Criar uma nova sala</h2>
-                    <form>
+                    <form onSubmit={handleCreateRoom}>
                         <input
                             type="text"
                             placeholder="Nome da sala"
+                            onChange={event => setNewRoom(event.target.value)}
+                            value={newRoom}
                         />
                         <Button type="submit">
                             Criar sala
