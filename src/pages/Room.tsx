e71f495ce983e168fb94cd1e29bd5df8e36bfd32
Igor Martins <iguinhoigor4@gmail.com>
2021-06-25 23:51:57 -0300

    feat: Estilizar componente de código da sala

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,6 +1,7 @@
 import logoImg from '../assets/images/logo.svg';
 
 import { Button } from '../components/Button';
+import { RoomCode } from './RoomCode';
 
 import '../styles/room.scss';
 
@@ -10,7 +11,7 @@ export function Room() {
             <header>
                 <div className="content">
                     <img src={logoImg} alt="Letmeask" />
-                    <div>código</div>
+                    <RoomCode />
                 </div>
             </header>
             
